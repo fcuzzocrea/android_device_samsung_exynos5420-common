5849a3a85251dc6b71661868f7fb001b4fa539f4
Francescodario Cuzzocrea <bosconovic@gmail.com>
2021-02-19 12:17:35 +0100

    exynos5420: remove double renderscript

--- exynos5420.mk
+++ exynos5420.mk
@@ -49,7 +49,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl.exynos5420 \
     camera.universal5420 \
     libshim_camera \
-    libhwjpeg
+    libhwjpeg \
+    Snap
 
 # Configstore
 PRODUCT_PACKAGES += \
@@ -73,7 +74,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.renderscript@1.0-impl \
     gralloc.exynos5 \
     libhwc2on1adapter \
     libion \
